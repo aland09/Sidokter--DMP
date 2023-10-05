f8e63a727cac4c829e2daa967ad1c5dab91164c1
Yoga Adi <yogaadi@Yogas-MacBook-Pro.local>
2023-10-05 19:02:50 +0700

    logo sidebar

--- resources/views/_layout/nav.blade.php
+++ resources/views/_layout/nav.blade.php
@@ -2,7 +2,7 @@
     <!-- Logo Start -->
     <div class="logo position-relative">
         <a href="{{ route('beranda') }}">
-            <img class="w-50" src="/img/logo/logo-alt.png" alt="logo" />
+            <img class="w-50" src="{{ asset('img/logo/logo-alt.png') }}" alt="logo" />
         </a>
     </div>
     <!-- Logo End -->
