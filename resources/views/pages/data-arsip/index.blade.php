77016cc20705e79995eb4ef84098d0a145349b6e
Yoga Adi <yoga.adi21@gmail.com>
2024-01-24 13:47:16 +0700

    try fix

--- resources/views/pages/data-arsip/index.blade.php
+++ resources/views/pages/data-arsip/index.blade.php
@@ -326,7 +326,7 @@
 
             $(document).on('click', '.btn-edit-parent', function() {
                 const item = $(this).data('item');
-                $('#form_edit_parent').attr('action', '/data-arsip.index/' + item['id']);
+                $('#form_edit_parent').attr('action', '/data-arsip/' + item['id']);
                 $(".modal-body #parent_kode_klasifikasi").val(item['kode_klasifikasi']);
                 $(".modal-body #parent_uraian").val(item['uraian']);
                 $(".modal-body #parent_tanggal_validasi").val(item['tanggal_validasi']);
